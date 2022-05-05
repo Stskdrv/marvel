c67fb672f8399907ef30048c0b6a1ee556b37984
Stanislav Kadyrov <stanislav_kadyrov@epam.com>
2022-05-06 02:23:15 +0300

    150 done

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,9 @@
 import './randomChar.scss';
 
 import { Component } from 'react';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 const id = Math.floor(Math.random() * (1011400-1011000) + 1011000);
@@ -14,41 +16,39 @@ class RandomChar extends Component {
 
     state ={
         char: {},
+        loading:true,
+        error: false,
     }
 
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({char, loading: false,})
+    }
+
+    onError = () => {
+        this.setState({loading: false, error: true,})
     }
 
     updateChar = (id) => {
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError)
     }
 
     render() {
-        const {char:{name, description, thumbnail, homePage, wiki}} = this.state;
+        const {char, error, loading} = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char} /> : null;
+
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homePage} className="button button__main">
-                                <div className="inner">Home Page</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -65,6 +65,33 @@ class RandomChar extends Component {
             </div>
         )
     }
+}
+
+const View = ({char}) => {
+    const {name, description, thumbnail, homePage, wiki} = char;
+
+    const modifideDescr = description.length > 180 ? description.slice(0,180)+'...' : description;
+
+    console.log(description.length, modifideDescr);
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {modifideDescr}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homePage} className="button button__main">
+                        <div className="inner">Home Page</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -28,7 +28,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description.lenth > 60 ? char.description.slice(60)+'...' : char.description,
+            description: char.description,
             thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
             homePage: char.urls[0].url,
             wiki:  char.urls[1].url,
