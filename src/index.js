7cfc3f223d30a39707ec64e33fab60544d7872e8
Stanislav Kadyrov <stanislav_kadyrov@epam.com>
2022-05-06 01:35:27 +0300

    149 done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import './style/style.scss';
+
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/App';
-import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getCharacter(1011052).then((res) => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
